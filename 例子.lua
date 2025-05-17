72c2bf87e8c0dfb47727a222dc853268560a1c56
DevSloPo <1342087454@qq.com>
2025-05-17 19:02:19 +0800

    Add files via upload

---
+++ 例子.lua
@@ -0,0 +1,3 @@
+local Httadmin = loadstring(game:HttpGet("https://raw.githubusercontent.com/DevSloPo/Notifica/refs/heads/main/Main.lua"))()
+
+Httadmin.send("欢迎", "看到我的专属通知了吗", 4, "rbxassetid://6031071050")
