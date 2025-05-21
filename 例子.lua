2d240225bf33b71539e0f1be6370a5dd7c026bd4
DevSloPo <1342087454@qq.com>
2025-05-21 19:48:42 +0800

    Update 例子.lua

--- 例子.lua
+++ 例子.lua
@@ -1,3 +1,3 @@
-local Httadmin = loadstring(game:HttpGet("https://raw.githubusercontent.com/DevSloPo/Notifica/refs/heads/main/Main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DevSloPo/Notifica/refs/heads/main/Main.lua"))()
 
-Httadmin.send("欢迎", "看到我的专属通知了吗", 4, "rbxassetid://6031071050")
+Httadmin.send("欢迎", "专属通知", 4, "rbxassetid://6031071050")
